5ef3f94732158bbc8e3c49f62dfd0964118ab0f1
JoubertVictor <johnvict323@gmail.com>
2024-11-12 01:07:10 -0300

    Aula 4

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,31 @@
+import "./App.css"
+import { Button } from "antd";
+import { DockerOutlined, DownloadOutlined } from "@ant-design/icons";
+
 function App() {
 
   return (
+
     <div>
+      <div className="div-teste">
+        <button> Botao 1 </button>
+        <button> Botao 2 </button>
+        <button> Botao 3 </button>
+      </div>
       <input>
       </input>
-      <button onClick={() => alert("Apertando pela arron")}>
+      <div className="div-botao">
+      <button
+        className="botao-teste"
+        onClick={() => alert("Apertando pela arron")}>
         Teste botao
       </button>
+      </div>
+      <div>
+        <Button type="primary" size="large" shape="round" icon={<DownloadOutlined/>} className="botao-antd">
+          Baixar
+        </Button>
+      </div>
     </div>
   )
 }
