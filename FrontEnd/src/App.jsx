30fd259411fc6a43be1fa1b3177776b9f4217dac
JoubertVictor <johnvict323@gmail.com>
2024-11-12 02:27:39 -0300

    começando aula 8

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import { Button } from "antd";
 import { DockerOutlined, DownloadOutlined } from "@ant-design/icons";
+import { DivTeste2 } from "./Style";
 
 function App() {
 
@@ -15,19 +16,22 @@ function App() {
       <input>
       </input>
       <div className="div-botao">
-      <button
-        className="botao-teste"
-        onClick={() => alert("Apertando pela arron")}>
-        Teste botao
-      </button>
+        <button
+          className="botao-teste"
+          onClick={() => alert("Apertando pela arron")}>
+          Teste botao
+        </button>
       </div>
       <div>
-        <Button type="primary" size="large" shape="round" icon={<DownloadOutlined/>} className="botao-antd">
+        <Button type="primary" size="large" shape="round" icon={<DownloadOutlined />} className="botao-antd">
           Baixar
         </Button>
       </div>
+      <DivTeste2>
+        OLA, to no style
+      </DivTeste2>
     </div>
   )
 }
- 
+
 export default App
---
+++ FrontEnd/src/Style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const DivTeste2 = styled.div`
+    width: 50%;
+    height: 80%;
+    margin-left: 40px;
+    border: solid;
+    background-color: green;
+`
